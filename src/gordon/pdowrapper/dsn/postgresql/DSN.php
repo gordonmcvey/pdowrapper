ed3e19150d2791a82594be93c5608293eeda8b72
gordonmcvey <grj.mcvey@gmail.com>
2023-07-31 21:04:17 +0100

    Added DSN for Postgres

---
+++ src/gordon/pdowrapper/dsn/postgresql/DSN.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace gordon\pdowrapper\dsn\postgresql;
+
+use gordon\pdowrapper\dsn\DSN as AbstractDSN;
+use ValueError;
+
+/**
+ * DSN for PostgreSQL connections
+ *
+ * @package gordon\pdowrapper\dsn\mysql
+ * @license https://www.apache.org/licenses/LICENSE-2.0
+ */
+final class DSN extends AbstractDSN
+{
+    private const DRIVER_PREFIX = "pgsql";
+
+    /**
+     * DSN constructor
+     *
+     * Note: The PostgreSQL DSN allows you to specify a username and a password, however this can also be specified via
+     * the Connection Spec.  If user login details are specified in both places then the login details in the DSN take
+     * precedence, with the values in the connection spec ignored.
+     *
+     * @param string $host Can specify either a domain name, or a UNIX socket
+     * @param int|null $port
+     * @param string|null $dbName
+     * @param string|null $user
+     * @param string|null $password
+     * @param string|null $sslmode
+     * @throws ValueError
+     * @todo $sslmode is not well documented, maybe better implemented as an enum?
+     */
+    public function __construct(
+        string  $host,
+        ?int    $port,
+        ?string $dbName,
+        ?string $user,
+        ?string $password,
+        ?string $sslmode
+    )
+    {
+        // Input validation
+        $this->validatePort($port);
+
+        // DSN initialisation
+        parent::__construct(self::DRIVER_PREFIX . ":" . $this->buildParamString([
+            "host"     => $host,
+            "port"     => $port,
+            "dbname"   => $dbName,
+            "user"     => $user,
+            "password" => $password,
+            "sslmode"  => $sslmode,
+        ]));
+    }
+}
---
+++ tests/unit/gordon/pdowrapper/dsn/postgresql/DSNTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace gordon\pdowrapper\tests\unit\dsn\postgresql;
+
+use gordon\pdowrapper\dsn\postgresql\DSN;
+use PHPUnit\Framework\TestCase;
+
+class DSNTest extends TestCase
+{
+
+}
