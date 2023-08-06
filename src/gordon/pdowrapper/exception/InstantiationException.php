bdebb25fbcebf63415d2a324564ac613ca873f57
gordonmcvey <grj.mcvey@gmail.com>
2023-08-06 13:42:00 +0100

    Added InstantiationException

---
+++ src/gordon/pdowrapper/exception/InstantiationException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace gordon\pdowrapper\exception;
+
+/**
+ *
+ * @package gordon\pdowrapper\exception
+ * @license https://www.apache.org/licenses/LICENSE-2.0
+ */
+class InstantiationException extends PDOException
+{
+}
